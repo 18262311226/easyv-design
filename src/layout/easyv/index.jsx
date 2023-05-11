20fdd04d0ebdb410f7220c34f1388ddb9264a57a
Gerfory <3435516833@qq.com>
2023-05-11 18:02:39 +0800

    首页页面搭建

--- src/layout/easyv/index.jsx
+++ src/layout/easyv/index.jsx
@@ -1,9 +1,71 @@
-import { useEffect } from 'react'
+import { useEffect,createElement } from 'react'
+import { UploadOutlined, UserOutlined, VideoCameraOutlined } from '@ant-design/icons';
+import { Layout, Menu, theme } from 'antd';
+const { Header, Content, Footer, Sider } = Layout;
 import styles from './index.module.less'
 
 function Easyv () {
+    const {
+        token: { colorBgContainer },
+    } = theme.useToken();
     return (
-        <div className={styles.container}>easyv</div>
+        <Layout style={{height:'100%'}}>
+            <Sider
+                breakpoint="lg"
+                collapsedWidth="0"
+                onBreakpoint={(broken) => {
+                    console.log(broken);
+                }}
+                onCollapse={(collapsed, type) => {
+                    console.log(collapsed, type);
+                }}
+            >
+                <div className={styles.logo} />
+                <Menu
+                    theme="dark"
+                    mode="inline"
+                    defaultSelectedKeys={['4']}
+                    items={[UserOutlined, VideoCameraOutlined, UploadOutlined, UserOutlined].map(
+                        (icon, index) => ({
+                            key: String(index + 1),
+                            icon: createElement(icon),
+                            label: `nav ${index + 1}`,
+                        }),
+                    )}
+                />
+            </Sider>
+            <Layout>
+                <Header
+                    style={{
+                        padding: 0,
+                        background: colorBgContainer,
+                    }}
+                />
+                <Content
+                    style={{
+                        height:'100%',
+                        margin: '24px 16px 0',
+                    }}
+                >
+                <div
+                    style={{
+                        padding: 24,
+                        height: '100%',
+                        background: colorBgContainer,
+                    }}
+                >
+                    content
+                </div>
+                </Content>
+                <Footer
+                    style={{
+                        textAlign: 'center',
+                    }}
+                >
+                    Ant Design ©2023 Created by Ant UED
+                </Footer>
+            </Layout>
+        </Layout>
     )
 }
 
