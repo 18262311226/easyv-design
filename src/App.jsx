e16a3a9eae7bce93e9c9041682c01d46d05c9ed7
Gerfory <3435516833@qq.com>
2023-05-12 15:20:04 +0800

    增加新增应用窗口

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import { useState } from 'react'
 import Easyv from './layout/easyv'
 import Login from './layout/login'
+import Template from './layout/template'
 import { Routes, Route, Navigate} from 'react-router-dom'
 import './App.css'
 
@@ -12,6 +13,7 @@ function App() {
       <Routes>
         <Route path="/login" element={<Login></Login>}></Route>
         <Route path="/Easyv" element={<Easyv></Easyv>}></Route>
+        <Route path="/template" element={<Template></Template>}></Route>
         <Route path="/" element={<Navigate to="/login" replace={false}></Navigate>}></Route>
         <Route path="*" element={<div>没有页面</div>}></Route>
       </Routes>
--- src/layout/easyv/index.jsx
+++ src/layout/easyv/index.jsx
@@ -1,6 +1,7 @@
-import { useEffect,createElement } from 'react'
+import { useEffect,createElement, useState } from 'react'
+import { useNavigate } from 'react-router-dom'
 import { AreaChartOutlined, ClockCircleOutlined, ContainerOutlined, WindowsOutlined,SettingOutlined,EditOutlined,EllipsisOutlined } from '@ant-design/icons';
-import { Layout, Menu, theme,Avatar, Card, Button } from 'antd';
+import { Layout, Menu, theme,Avatar, Card, Button, Empty, List, Modal, Form, Input } from 'antd';
 const { Header, Content, Sider } = Layout;
 const { Meta } = Card;
 import styles from './index.module.less'
@@ -16,18 +17,42 @@ function getItem(label, key, icon, children, type) {
 }
 
 function Easyv () {
+    const navigate = useNavigate()
+    const [isModalOpen, setModalOpen] = useState(false)
+
     const {
         token: { colorBgContainer },
-    } = theme.useToken();
+    } = theme.useToken()
+
     const items = [
         getItem('我的应用', '1', <AreaChartOutlined />),
         getItem('最近编辑', '2', <ClockCircleOutlined />),
         getItem('回收站', '3', <ContainerOutlined />)
-    ];
+    ]
+
+    const data = [1, 2, 3, 4, 5]
+
+    const createNewApp = () => {
+        setModalOpen(!isModalOpen)
+    }
 
     const onClick = ({ key }) => {
        
-    };
+    }
+
+    const handleOk = () => {
+        navigate('/template')
+        setModalOpen(false)
+    }
+
+    const handleCancel = () => {
+        setModalOpen(false)
+    }
+
+    const editTemplate = () => {
+        navigate('/template')
+    }
+
     return (
         <Layout style={{height:'100%'}}>
             <Sider
@@ -63,34 +88,78 @@ function Easyv () {
                     >
                         <div className={styles.operation}>
                             <div className={styles.title}>我的应用</div>
-                            <Button className={styles.createBtn} type="primary">新建应用</Button>
+                            <Button className={styles.createBtn} type="primary" onClick={createNewApp}>新建应用</Button>
                         </div>
-                        <Card
-                            hoverable
-                            style={{
-                                width: 300
-                            }}
-                            cover={
-                                <img
-                                    alt="example"
-                                    src="https://img1.baidu.com/it/u=1509990960,2346188312&fm=253&app=138&size=w931&n=0&f=JPEG&fmt=auto?sec=1683997200&t=8b507cf6083bc24c82db76e9cda58c74"
-                                />
+                        <div className={styles.cards}>
+                            {
+                                data.length ? (
+                                    <List
+                                        grid={{ gutter: 16, column: 4 }}
+                                        dataSource={data}
+                                        renderItem={(item) => (
+                                            <List.Item>
+                                                <Card
+                                                    hoverable
+                                                    className={styles.cardbox}
+                                                    cover={
+                                                        <img
+                                                            alt="example"
+                                                            src="https://img1.baidu.com/it/u=1509990960,2346188312&fm=253&app=138&size=w931&n=0&f=JPEG&fmt=auto?sec=1683997200&t=8b507cf6083bc24c82db76e9cda58c74"
+                                                        />
+                                                    }
+                                                    actions={[
+                                                        <SettingOutlined key="setting" />,
+                                                        <EditOutlined key="edit" onClick={editTemplate}/>,
+                                                        <EllipsisOutlined key="ellipsis" />,
+                                                    ]}
+                                                >
+                                                    <Meta
+                                                        avatar={<Avatar src="https://zos.alipayobjects.com/rmsportal/ODTLcjxAfvqbxHnVXCYX.png" />}
+                                                        title="大屏可视化demo"
+                                                        description="未发布"
+                                                    />
+                                                </Card>
+                                            </List.Item>
+                                        )}
+                                    />
+                                ) : (
+                                    <Empty image={Empty.PRESENTED_IMAGE_SIMPLE} />
+                                )
                             }
-                            actions={[
-                                <SettingOutlined key="setting" />,
-                                <EditOutlined key="edit" />,
-                                <EllipsisOutlined key="ellipsis" />,
-                            ]}
-                        >
-                            <Meta
-                                avatar={<Avatar src="https://zos.alipayobjects.com/rmsportal/ODTLcjxAfvqbxHnVXCYX.png" />}
-                                title="大屏可视化demo"
-                                description="未发布"
-                            />
-                        </Card>
+                        </div>
                     </div>
                 </Content>
             </Layout>
+            <Modal title="create" open={isModalOpen} onOk={handleOk} onCancel={handleCancel}>
+                <Form
+                    layout="vertical"
+                    initialValues={{
+                        remember: true,
+                    }}
+                >
+                    <Form.Item
+                        name="appname"
+                        label="应用名称"
+                        rules={[
+                            {
+                                required: true,
+                                message: 'Please input your Username!',
+                            },
+                        ]}
+                    >
+                        <Input type="text" placeholder="请输入应用名称" />
+                    </Form.Item>
+                    <Form.Item
+                        label="简介"
+                        name="option"
+                    >
+                        <Input
+                            type="text"
+                            placeholder="请输入应用简介"
+                        />
+                    </Form.Item>
+                </Form>
+            </Modal>
         </Layout>
     )
 }
